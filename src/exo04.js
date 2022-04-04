5801c40d01a01bf3d0f9c38c8d1bd225ff89c109
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-04 16:26:49 +0200

    FEAT (Training) Résolution exo 4 Un peu plus tordu celui là, à noter que la variable propValue reste accessible dans le scope des getters et setters uniquement mais sont bien présents : aka pas besoin de définir une propriété supplémentaire dans l'objet pour porter la valeur.

--- src/exo04.js
+++ src/exo04.js
@@ -4,10 +4,51 @@ export function watchProperty(obj, prop, onRead, onWrite) {
   // - onWrite(newValue) lorsque la propriété prop est modifiée
   // ces fonctions sont juste des observateurs et n'ont pas de valeur de retour
   // or il doit toujours être possible d'écrire et de lire une valeur pour la propriété prop
+  let propValue = obj[prop];
+  Object.defineProperty(obj, prop, {
+    get: function () {
+      onRead();
+      return propValue;
+    },
+    set: function (value) {
+      onWrite(value);
+      propValue = value;
+    }
+  });
 }
 
 export function setPrivatesAndConstants(obj) {
   // changer le descripteur de chaque propriété de l'objet:
   // énumérable si la clé ne commence pas par par un _
   // mutable et configurable si la clé ne commence pas par une majuscule (regex: /[A-Z]/)
+  let reg = /[A-Z]/;
+  for (let key in obj) {
+    if (key[0] === "_") {
+      Object.defineProperty(obj, key, {
+        enumerable: false
+      });
+    } else if (reg.test(key[0])) {
+      Object.defineProperty(obj, key, {
+        writable: false,
+        configurable: false
+      });
+    }
+  }
+}
+
+/*
+  Solution plus élégante et plus concise
+*/
+export function setPrivatesAndConstants2(obj) {
+  // changer le descripteur de chaque propriété de l'objet:
+  // énumérable si la clé ne commence pas par par un _
+  // mutable et configurable si la clé ne commence pas par une majuscule (regex: /[A-Z]/)
+  let reg = /[A-Z]/;
+  for (let key in obj) {
+    Object.defineProperty(obj, key, {
+      enumerable: key[0] !== "_",
+      writable: !reg.test(key[0]),
+      configurable: !reg.test(key[0])
+    });
+  }
 }
