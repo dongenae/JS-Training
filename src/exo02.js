50333179fbbbb023cbfc8391183b19a189646d24
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-04 13:55:46 +0200

    FEAT (training) Résolution exo2 Différentes méthodes mais nuance importante sur les propriétés "propres" et "énumérables" ce qui fait qu'on ne prend pas forcément tout et ça faisait planter le test 1 (alors que le 2 fonctionne, différence d'implémentation du test en soit).

--- src/exo02.js
+++ src/exo02.js
@@ -1,14 +1,35 @@
 // retourne true si l'objet a une propriété ayant comme clé key
 export function hasProperty(obj, key) {
-  return true;
+  /* Mauvaise solution : si on fait manuellement : 
+      obj.key = undefined
+    alors la propriété existe encore mais pourtant le test renverra
+  false, le "in" semble plus représentatif de l'état réel de l'objet  */
+  // return obj[key] !== undefined;
+
+  /* Liste uniquement les clés propres et énumérables */
+  // return Object.keys(obj).indexOf(key) !== -1;
+  // return Object.keys(obj).includes(key);
+
+  return key in obj;
 }
 
 // retourne true si l'objet a une propriété ayant comme valeur value
 export function hasPropertyValue(obj, value) {
-  return true;
+  /* Liste uniquement les values propres et énumérables */
+  // return Object.values(obj).indexOf(value) !== -1;
+  // return Object.values(obj).includes(value);
+
+  for (let k in obj) {
+    if (obj[k] === value) {
+      return true;
+    }
+  }
+  return false;
 }
 
 // retourne le nombre de propriétés d'un objet (note: sans compter les propriétés déléguées)
 export function getNumberOfProperties(obj) {
-  return 0;
+  return Object.keys(obj).length;
+  // return Object.values(obj).length;
+  // return Object.entries(obj).length;
 }
