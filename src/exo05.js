52cccc0592538d8dc1e410c58731cdcb3a2b0720
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-04 17:22:33 +0200

    FEAT (Training) Résolution exo 5 Voir commentaire, notion de closure avec "let" (scope local au tour de boucle) vs "var" (scope global de la fonction).

--- src/exo05.js
+++ src/exo05.js
@@ -8,11 +8,17 @@ const paroles = [
 export const instructions = [];
 
 // TOFIX: imprimer la chanson correctement
-for (var i = 0; i < paroles.length; i++) {
+for (let i = 0; i < paroles.length; i++) {
   instructions.push(function printNextLine() {
     return paroles[i] + ", " + paroles[i];
   });
 }
+
+/* Le "let" défini un scope pour le i limité au tour de boucle 
+en cours donc chaque tour de boucle va définir une closure avec 
+la valeur actuelle du i. Tandis que le "var" définit un i global 
+à tous les tours de boucle donc chaque fonction générée par le for 
+pointera sur le même objet en mémoire. */
 
 // code de test, à essayer en console pour voir le problème
 /*instructions.forEach(function (printNextLine) {
