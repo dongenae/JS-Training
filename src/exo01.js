d304dae08c0453508a304b141206bfbb1c9e2237
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-04 11:10:13 +0200

    FEAT (Training) Résolution exo1 A noter que null et undefined ont comme "typeof" object, ce qui induit en erreur, c'est donc un cas particulier à gérer dans une condition à part.

--- src/exo01.js
+++ src/exo01.js
@@ -1,4 +1,8 @@
 // retourne true si la variable passée est une primitive
 export function isPrimitive(x) {
-    return true;
+  return (
+    (typeof x !== "object" && typeof x !== "function") ||
+    x === undefined ||
+    x === null
+  );
 }
