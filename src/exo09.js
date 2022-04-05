9a40c40af4cc13a5b288a477df0bdd85f9465b35
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-05 14:46:19 +0200

    FEAT (Training) Résolution exo 9 Un peu tordu mais assez simple au final, surtout la destructuration de paramètre.

--- src/exo09.js
+++ src/exo09.js
@@ -1,11 +1,23 @@
+// Retourner un nouvel objet avec les propriétés de l'objet data
+// et ces valeurs par défaut pour les propriétés manquantes :
+
 export function parseUserData(data) {
-  // TODO: retourner un nouvel objet avec les propriétés de l'objet data
-  // et ces valeurs par défaut pour les propriétés manquantes :
-  const defaults = { name: "Anonymous", isAdmin: false };
-
   // 1 - en utilisant Object.assign
-  // 2 - en utilisant l'opérateur spread sur les properties
-  // 3 - en utilisant le destructuring et les paramètres par défaut pour parseUserData
+  const defaults = { name: "Anonymous", isAdmin: false };
+  return Object.assign({}, defaults, data);
+}
 
-  return data;
+export function parseUserData2(data) {
+  // 2 - en utilisant l'opérateur spread sur les properties
+  const defaults = { name: "Anonymous", isAdmin: false };
+  return { ...defaults, ...data };
+}
+
+export function parseUserData3({
+  name = "Anonymous",
+  isAdmin = false,
+  ...props
+}) {
+  // 3 - en utilisant le destructuring et les paramètres par défaut pour parseUserData
+  return { name, isAdmin, ...props };
 }
