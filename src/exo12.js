54f337f64de4f2c1263810db4aedd237821aa1c0
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-05 17:17:01 +0200

    FEAT (Training) Résolution exo12 Pattern subsriber / publisher, intéressant pour les actions asynchrones.

--- src/exo12.js
+++ src/exo12.js
@@ -1,15 +1,35 @@
 export function PubSub() {
-  this.events = new Map()
+  this.events = new Map();
 }
 
-PubSub.prototype.on = function(event, callback) {
-  // TODO: enregistrer le callback à déclencher suite à l'événement `event`
+PubSub.prototype.on = function (event, callback) {
+  // Enregistrer le callback à déclencher suite à l'événement `event`
+  if (this.events.get(event)) {
+    this.events.get(event).push(callback);
+  } else {
+    this.events.set(event, [callback]);
+  }
+  // ou avec un opérateur ternaire
 };
 
-PubSub.prototype.emit = function(event, data) {
-  // TODO: appeler les callbacks enregistrés pour l'événement `event`
+PubSub.prototype.emit = function (event, data) {
+  // Appeler les callbacks enregistrés pour l'événement `event`
+  this.events.get(event).forEach((callback) => callback(data));
 };
 
 // Pour aller plus loin:
 // - méthode off(event, callback) pour retirer une souscription
-// - on('*') pour réagir à tous les événements
+// - on('*') pour réagir à tous les événements => pas trop compris l'énoncé
+
+PubSub.prototype.off = function (event, callback) {
+  const eventConcerned = this.event.get(event);
+  if (eventConcerned) {
+    let newCallbacks = [];
+    eventConcerned.forEach(function (myCallback) {
+      if (callback !== myCallback) {
+        newCallbacks.push(myCallback);
+      }
+    });
+    this.event.set(event, newCallbacks);
+  }
+};
