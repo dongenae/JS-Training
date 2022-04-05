2f88bab019740d34cb57e189aa91009e20e762c5
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-05 16:18:44 +0200

    FEAT (Training) Résolution exo 11 Utilisation des maps et des sets (qui ont des règles d'unicité et sont faiblement typés donc permettent de faire beaucoup de choses par rapport au java)

--- src/exo11.js
+++ src/exo11.js
@@ -1,13 +1,15 @@
 export function deduplicateArray(arr) {
-  // TODO: retirer les valeurs en double dans l'array et retourner l'array filtrée
+  // Retirer les valeurs en double dans l'array et retourner l'array filtrée
   // 1 - avec les méthodes filter et indexOf
+  return arr.filter((elem, indexEnCours) => arr.indexOf(elem) === indexEnCours);
   // 2 - avec un Set et l'opérateur spread
-  return arr;
+  return [...new Set(arr)];
 }
 
 export function getPropertyFromValue(obj, val) {
-  // TODO: retourner la clé de la propriété de obj ayant comme valeur val
+  // Retourner la clé de la propriété de obj ayant comme valeur val
   // 1 - avec les méthodes find et Object.keys
+  return Object.keys(obj).find((key) => obj[key] === val);
   // 2 - avec une Map et Object.entries
-  return null;
+  return new Map(Object.entries(obj).map((entry) => entry.reverse())).get(val);
 }
