c0d865b0fe5ad27be3fcf87a5798aa05c6a505a9
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-06 11:40:45 +0200

    FEAT (Training) Exo 15 mise en place de prototype Attention la POO en js n'est pas comme en java, le prototype "délègue" mais n'initialise pas l'objet, ce n'est pas un "parent" à proprement parler. Ce n'est donc pas du tout la même relation.

--- src/exo15.js
+++ src/exo15.js
@@ -6,20 +6,41 @@ export const person = {
   }
 };
 
-// TODO : assigner person comme prototype
+// Assigner person comme prototype
+
+// 1) avec Object.setPrototypeOf
 export const user = {
   rights: [],
   hasRight(right) {
     return this.rights.includes(right);
   }
 };
+Object.setPrototypeOf(user, person);
 
-// TODO: assigner user comme prototype
+// 2) avec Object.create et avec Object.assign
+/* export const user = Object.create(person);
+Object.assign(user, {
+  rights: [],
+  hasRight(right) {
+    return this.rights.includes(right);
+  }
+});
+*/
+
+// Assigner user comme prototype
+
+// 1) avec Object.setPrototypeOf
 export const bob = {
   name: "Afett",
   first: "Bob",
   rights: ["create", "read"]
 };
+Object.setPrototypeOf(bob, user);
 
-// 1) avec Object.setPrototypeOf
 // 2) avec Object.create et avec Object.assign
+/* export const bob = Object.create(user);
+Object.assign(bob, {
+  name: "Afett",
+  first: "Bob",
+  rights: ["create", "read"]
+}); */
