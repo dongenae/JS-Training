95f6e5607ee5eb30f9c768bccfb5caed29c9dabe
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-06 15:59:13 +0200

    FEAT (exo 19) Mise en place de proxy Utilisation de proxy pour surcharger le comportement des fonctions, très élégant comme manière de coder.

--- src/exo19.js
+++ src/exo19.js
@@ -1,8 +1,18 @@
 export const addAliasForProperties = (object, alias) => {
-  // TODO: retourner un Proxy pour l'objet permettant
+  // Retourner un Proxy pour l'objet permettant
   // de déclarer des alias pour accéder en lecture ou écriture
   // à une propriété de l'objet
-  return object;
+  return new Proxy(object, {
+    get(obj, prop) {
+      return Reflect.get(obj, prop in alias ? alias[prop] : prop);
+    },
+    set(obj, prop, valeur) {
+      return Reflect.set(obj, prop in alias ? alias[prop] : prop, valeur);
+    },
+    has(obj, prop) {
+      return Reflect.has(obj, prop) || Reflect.has(obj, alias[prop]);
+    }
+  });
 };
 
 // exemple d'utilisation:
@@ -13,15 +23,24 @@ const user = addAliasForProperties(
 
 // `${user.firstName} ${user.lastName}` === "Rick Sanchez"
 
-export const countFunctionCalls = fn => {
-  // TODO: retourner un Proxy pour la fonction permettant
+export const countFunctionCalls = (fn) => {
+  // Retourner un Proxy pour la fonction permettant
   // de compter le nombre d'appels faits pour cette fonction,
   // à récupérer via fn.count
-  return fn;
+  let compteur = 0;
+  return new Proxy(fn, {
+    apply(fn, thisArg, tabArgs) {
+      ++compteur;
+      return Reflect.apply(fn, thisArg, tabArgs);
+    },
+    get(fn, prop) {
+      return prop === "count" ? compteur : Reflect.get(fn, prop);
+    }
+  });
 };
 
 // exemple d'utilisation:
-const fn = countFunctionCalls(n => n * 2);
+const fn = countFunctionCalls((n) => n * 2);
 fn(1);
 fn(2);
 fn(999);
