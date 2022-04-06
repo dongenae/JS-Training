98be00b03e374c977fdc0031ebcf8575ae1919b5
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-06 09:47:09 +0200

    FEAT (Training) Exo 13 promisify callback

--- src/exo13.js
+++ src/exo13.js
@@ -1,13 +1,34 @@
 // fn prend en premier argument un callback
 // fn(function callback(error, result){ ... }, arg1, arg2);
 export function promisify(fn) {
-  // TODO: retourner une fonction appelant fn mais
+  // Retourner une fonction appelant fn mais
   // retournant une Promise au lieu de passer un callback
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      fn(function callback(error, result) {
+        error ? reject(error) : resolve(result);
+      }, ...args);
+    });
+  };
+  /* NB : pas super utile à savoir en pratique mais intéressant,
+   ce qu'il faut retenir c'est qu'on peut convertir des callbacks 
+  en Promise qui sont beaucoup plus pratiques */
 }
 
 // exemple d'utilisation
 const wait = promisify(setTimeout);
-wait(1000)
-  .then(() => { console.log("1"); return wait(1000) })
-  .then(() => { console.log("2"); return wait(1000) })
-  .then(() => { console.log("3") });
+wait(100)
+  .then(() => {
+    console.log("1");
+    return wait(100);
+  })
+  .then(() => {
+    console.log("2");
+    return wait(100);
+  })
+  .then(() => {
+    console.log("3");
+  })
+  .catch((error) => {
+    console.error("Erreur: " + error);
+  });
