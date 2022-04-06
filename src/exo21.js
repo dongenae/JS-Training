a3d18bc56e2a5bdc5e5ea449c6fa13e985f5bb98
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-06 17:12:48 +0200

    FEAT (exo 21) Programmation fonctionnelle avec fonctions composées Manière plus légère de coder en fonctionnelle, celle qu'on a vu dans l'exo 20 (API chainée) est pratique mais alourdi beaucoup les objets. Ici c'est un peu plus léger.

--- src/exo21.js
+++ src/exo21.js
@@ -1,25 +1,37 @@
 import data from "../__tests__/fakedata.json";
 
-// TODO: implémenter une fonction qui retourne la composée de plusieurs fonctions
+// Implémenter une fonction qui retourne la composée de plusieurs fonctions
 // compose(f,g,h)(...args) <=> h(g(f(...args)))
-export function compose() {
+export function compose(firstFunction, ...otherFunctions) {
+  if (otherFunctions.length === 0) return firstFunction;
+  return function (...args) {
+    return compose(...otherFunctions)(firstFunction(...args));
+  };
+}
 
+// Equivalent sans récursivité avec un reduce
+export function compose2(firstFunction, ...otherFunctions) {
+  return (...args) =>
+    otherFunctions.reduce(
+      (result, nextFunction) => nextFunction(result),
+      firstFunction(...args)
+    );
 }
 
 // les fonctions composées doivent être pures et non mutables
-export const where = (field, condition, array) => array.filter(({ [field]: value }) => condition(value))
-export const orderBy = (field, array) => [...array].sort((a, b) => a[field] < b[field] ? -1 : 1);
-export const take = (number, array) => array.slice(0, number)
-export const map = (transform, array) => array.map(transform)
+export const where = (field, condition, array) =>
+  array.filter(({ [field]: value }) => condition(value));
+export const orderBy = (field, array) =>
+  [...array].sort((a, b) => (a[field] < b[field] ? -1 : 1));
+export const take = (number, array) => array.slice(0, number);
+export const map = (transform, array) => array.map(transform);
 
 // exemple d'utilisation
 const query = compose(
-    data => where("age", n => n >= 18, data),
-    results => orderBy("lastName", results),
-    results => take(5, results),
-    results => map(user => `${user.firstName} ${user.lastName}`, results)
-)
-
-console.log(
-    query(data).join("\n")
+  (data) => where("age", (n) => n >= 18, data),
+  (results) => orderBy("lastName", results),
+  (results) => take(5, results),
+  (results) => map((user) => `${user.firstName} ${user.lastName}`, results)
 );
+
+console.log(query(data).join("\n"));
