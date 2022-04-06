2db2e04f3d795ae53ecaac1c1be4c2d61ac42d47
dongenae <51057973+dongenae@users.noreply.github.com>
2022-04-06 16:45:38 +0200

    FEAT (exo 20) API chainable en programmation fonctionnelle Programmation orienté fonctionnelle avec récursivité dans le cas d'une API chainable (c'est comme ça que sont implémentées les libs JS JQuery & underscore).

--- src/exo20.js
+++ src/exo20.js
@@ -1,24 +1,31 @@
 import data from "../__tests__/fakedata.json";
 
-export const query = array =>
+export const query = (array) =>
   Object.assign(array, {
     where(key, condition) {
-      //TODO: filtrer les éléments selon une condition sur une propriété
+      // Filtrer les éléments selon une condition sur une propriété
+      return query(array.filter((person) => condition(person[key])));
     },
     orderBy(key) {
-      //TODO: trier les éléments selon une propriété
+      // Trier les éléments selon une propriété
+      return query(
+        [...array].sort((a, b) => {
+          return a[key].localeCompare(b[key]);
+        })
+      );
     },
     take(number) {
-      //TODO: retourner les N premiers éléments de la liste
+      // Retourner les N premiers éléments de la liste
+      return query([...array].slice(0, number));
     }
   });
 
 // exemple d'utilisation
 console.log(
   query(data)
-    .where("age", n => n >= 18)
+    .where("age", (n) => n >= 18)
     .orderBy("lastName")
     .take(5)
-    .map(user => `${user.firstName} ${user.lastName}`)
+    .map((user) => `${user.firstName} ${user.lastName}`)
     .join("\n")
 );
